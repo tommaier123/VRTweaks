9fd97e2bb1958caca867b15df32633fdf4fd6358
EthanFischer <ethan.f.fischer@gmail.com>
2020-12-13 15:24:15 -0600

    GazeCursorEnabler

---
+++ VRTweaks/GazeCursorEnabler.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace VRTweaks
+{
+    [HarmonyPatch(typeof(VROptions))]
+    [HarmonyPatch("GetUseGazeBasedCursor")]
+    public static class GazeCursorEnabler
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
