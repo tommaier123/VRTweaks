d9cfaa6a3ccbe946dce85e9113cc37ce5858d88f
EthanFischer <ethan.f.fischer@gmail.com>
2021-02-16 18:39:57 -0600

    Fix weird positioning thing

--- VRTweaks/SnapTurn/SnapTurning.cs
+++ VRTweaks/SnapTurn/SnapTurning.cs
@@ -6,7 +6,7 @@ using UnityEngine.XR;
 
 namespace VRTweaks.SnapTurn
 {
-    [HarmonyPatch(typeof(MainCameraControl))]
+    [HarmonyPatch(typeof(global::MainCameraControl))]
     [HarmonyPatch("Update")]
     public static class SnapTurning
     {
@@ -26,8 +26,11 @@ namespace VRTweaks.SnapTurn
         }
 
         [HarmonyPrefix]
-        public static bool Prefix()
+        public static bool Prefix(MainCameraControl __instance, out Vector3 __state)
         {
+            __state = __instance.viewModel.transform.localPosition;
+            Debug.Log($"prefix LocalPosition {__state}");
+
             if (!Config.EnableSnapTurning)
             {
                 return true; //Enter vanilla method
@@ -82,6 +85,13 @@ namespace VRTweaks.SnapTurn
             }
 
             return newEulerAngles;
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix(MainCameraControl __instance, Vector3 __state)
+        {
+            Debug.Log($"LocalPosition {__state}");
+            __instance.viewModel.transform.localPosition = __state;
         }
     }
 }
