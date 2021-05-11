8113aaecc63defd93775668b310a4d0b67c374e5
Ethan Fischer <ethan.f.fischer@gmail.com>
2021-05-11 17:07:58 +0800

    Add recenter button to pause menu (#12)
    * Intial
    
    * Removed Some comment
    
    * Removed unneeded code
    
    * Added Gazecursor Fix
    
    * Wboit Fixes
    
    * Update WBOITFixes.cs
    
    * Sync with Main Master
    
    * Delete gazeLayerFix.cs
    
    * Delete Config.cs
    
    * Updated
    
    * Put uwexr.csproj back
    
    * Put SnapTurning.cs whitespace back
    
    * Copy recenter button from VREnhancements
    
    * Put main.cs and WBOITFixes.cs back as they are on master
    
    Co-authored-by: POT HEAD <jeremy420dashyt@msn.com>
    Co-authored-by: ihatetn931 <shadowpot@gmail.com>
    Co-authored-by: Nova_Max <40432491+tommaier123@users.noreply.github.com>

---
+++ VRTweaks/RecenterVRButtonAdder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.UI;
+using HarmonyLib;
+using System.Linq;
+using TMPro;
+
+[HarmonyPatch(typeof(IngameMenu), nameof(IngameMenu.Awake))]
+public static class RecenterVRButtonAdder
+{
+    private static Button recenterVRButton;
+
+    [HarmonyPostfix]
+    public static void Postfix(IngameMenu __instance)
+    {
+        if (__instance != null && recenterVRButton == null)
+        {
+            //I think this is copying an existing button
+            Button menuButton = __instance.quitToMainMenuButton.transform.parent.GetChild(0).gameObject.GetComponent<Button>();
+            recenterVRButton = UnityEngine.Object.Instantiate<Button>(menuButton, __instance.quitToMainMenuButton.transform.parent);
+            recenterVRButton.transform.SetSiblingIndex(1);//put the button in the second position in the menu
+            recenterVRButton.name = "RecenterVR";
+            recenterVRButton.onClick.RemoveAllListeners();//this seems to be removing listeners that would have been copied from the original button
+                                                          //add new listener
+            recenterVRButton.onClick.AddListener(delegate ()
+            {
+                VRUtil.Recenter();
+            });
+            //might be a better way to replace the text of the copied button
+            var enumerable = recenterVRButton.GetComponents<TextMeshProUGUI>().Concat(recenterVRButton.GetComponentsInChildren<TextMeshProUGUI>());
+            foreach (var text in enumerable)
+            {
+                text.text = "Recenter VR";
+            }
+        }
+    }
+}
