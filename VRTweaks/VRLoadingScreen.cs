55f42051f79c09100d9d513d472a7106117d21a2
brandonmousseau <52470509+brandonmousseau@users.noreply.github.com>
2021-05-12 22:24:30 -0700

    Add better loading screen (#15)
    * Add better loading screen
    
    This creates a loading screen that isn't tied to the player's face. It
    will float a bit in front of the player. You can look around and see the
    world loading around you as it is currently implemented - but it is for
    sure better than what happens by default.
    
    * Fix patch class name

---
+++ VRTweaks/VRLoadingScreen.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using HarmonyLib;
+
+namespace VRTweaks
+{
+    class VRLoadingScreen
+    {
+
+        [HarmonyPatch(typeof(uGUI), nameof(uGUI.Update))]
+        class uGUI_LoadingScreenReposition_Patch
+        {
+
+            private static GameObject loadingCanvasObject;
+            private static Canvas loadingCanvas;
+
+            static void Postfix(uGUI __instance)
+            {
+                maybeCreateLoadingCanvas();
+                if (isLoading() && hasLoadingScreen(__instance))
+                {
+                    GameObject loadingScreen = __instance.loading.gameObject;
+                    loadingScreen.transform.SetParent(loadingCanvas.GetComponent<RectTransform>());
+                    loadingScreen.GetComponent<RectTransform>().position =
+                        MainCamera.camera.gameObject.transform.parent.position + MainCamera.camera.gameObject.transform.parent.forward * 3f;
+                    loadingScreen.GetComponent<RectTransform>().LookAt(MainCamera.camera.gameObject.transform.parent);
+                    loadingScreen.GetComponent<RectTransform>().Rotate(0f, 180f, 0f);
+                }
+            }
+
+            private static bool isLoading()
+            {
+                return uGUI.main.loading.IsLoading || !uGUI.main;
+            }
+
+            private static bool hasLoadingScreen(uGUI instance)
+            {
+                return instance.loading != null && instance.loading.gameObject != null;
+            }
+
+            private static void maybeCreateLoadingCanvas()
+            {
+                if (loadingCanvasObject != null && loadingCanvasObject)
+                {
+                    return;
+                }
+                loadingCanvasObject = new GameObject("VRLoadingCanvas");
+                UnityEngine.Object.DontDestroyOnLoad(loadingCanvasObject);
+                loadingCanvas = loadingCanvasObject.AddComponent<Canvas>();
+                loadingCanvas.renderMode = RenderMode.WorldSpace;
+                loadingCanvas.GetComponent<RectTransform>().SetParent(loadingCanvasObject.transform, false);
+            }
+        }
+
+        [HarmonyPatch(typeof(uGUI_SceneLoading), nameof(uGUI_SceneLoading.SetFastLoadMode))]
+        class uGUI_SceneLoading_SetFastLoadMode_Patch
+        {
+            static bool Prefix(uGUI_SceneLoading __instance, bool useFastLoadMode)
+            {
+                PlatformServices services = PlatformUtils.main.GetServices();
+                if (services != null)
+                {
+                    services.SetUseFastLoadMode(useFastLoadMode);
+                }
+                return false;
+            }
+        }
+
+    }
+}
