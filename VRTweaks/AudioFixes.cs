adff69ebcf98ca06eec74baa25401285bdc2ac36
brandonmousseau <52470509+brandonmousseau@users.noreply.github.com>
2021-05-12 22:24:17 -0700

    Fix audio spatialization (#14)

---
+++ VRTweaks/AudioFixes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using FMODUnity;
+
+namespace VRTweaks
+{
+    public class AudioFixes
+    {
+
+        [HarmonyPatch(typeof(SNCameraRoot), nameof(SNCameraRoot.Awake))]
+        public class AudioSpatializationFixes
+        {
+            static void Postfix(SNCameraRoot __instance, Camera ___mainCamera)
+            {
+                if (___mainCamera != null)
+                {
+                    Object.DestroyImmediate(__instance.gameObject.GetComponent<AudioListener>());
+                    Object.DestroyImmediate(__instance.gameObject.GetComponent<StudioListener>());
+                    ___mainCamera.gameObject.AddComponent<StudioListener>();
+                }
+            }
+        }
+
+    }
+}
