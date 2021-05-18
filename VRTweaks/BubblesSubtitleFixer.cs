456950a752159e217ea570cba396b301d98d446a
ihatetn931 <shadowpot@gmail.com>
2021-05-18 00:13:47 -0500

    Fixed Project File

--- VRTweaks/BubblesSubtitleFixer.cs
+++ VRTweaks/BubblesSubtitleFixer.cs
@@ -4,27 +4,27 @@ using UWE;
 
 namespace VRTweaks
 {
-	[HarmonyPatch(typeof(PlayerBreathBubbles), "Start")]
-	public static class PlayerBreathBubbles_Patch
-	{
-		[HarmonyPrefix]
-		public static bool Prefix(PlayerBreathBubbles __instance)
-		{
-           //Place the bubbles right at about neck level but does not rotate with view
+    [HarmonyPatch(typeof(PlayerBreathBubbles), "Start")]
+    public static class PlayerBreathBubbles_Patch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(PlayerBreathBubbles __instance)
+        {
+            //Place the bubbles right at about neck level but does not rotate with view
             __instance.anchor.position = new Vector3(0.0f, 1.6f, 0.0f);
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 
     //Dosen't seem to work in anything other then Update, need to find a way so I don't have to use hardcoded values
     [HarmonyPatch(typeof(Subtitles), "Update")]
-     public static class SubtitleFixer
-     {
-         [HarmonyPrefix]
-         public static bool Prefix(Subtitles __instance)
-         {
-             __instance.transform.parent.localPosition = new Vector3(-457.6f, -432.5f, 0.0f);
-             return true;
-         }
-     }
+    public static class SubtitleFixer
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(Subtitles __instance)
+        {
+            __instance.transform.parent.localPosition = new Vector3(-457.6f, -432.5f, 0.0f);
+            return true;
+        }
+    }
 }
--- VRTweaks/HUDFixer.cs
+++ VRTweaks/HUDFixer.cs
@@ -9,7 +9,7 @@ namespace VRTweaks
         [HarmonyPostfix]
         public static void Postfix(uGUI_HealthBar __instance)
         {
-            //     //Shift the healthbar, oxygen meter, and temperature parent UI element to the right
+            //Shift the healthbar, oxygen meter, and temperature parent UI element to the right
             __instance.transform.parent.localPosition += new Vector3(300, 0, 0);
             MiscSettings.SetUIScale(0.7f);
         }
